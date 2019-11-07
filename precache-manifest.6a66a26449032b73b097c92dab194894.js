c19ee5f3560837d582e97c87b0a22bdd369d9a69
shivamgupta94 <shivamgupta.me@gmail.com>
2019-11-07 13:38:32 -0500

    Updates

---
+++ precache-manifest.6a66a26449032b73b097c92dab194894.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a017ee88a130a36cbd5e135095e3bf0f",
+    "url": "/checkers-react/index.html"
+  },
+  {
+    "revision": "742c438ce640cfe5f0a2",
+    "url": "/checkers-react/static/css/main.4f7e95de.chunk.css"
+  },
+  {
+    "revision": "6d7dad025d2ebeccbbdd",
+    "url": "/checkers-react/static/js/2.52108a72.chunk.js"
+  },
+  {
+    "revision": "742c438ce640cfe5f0a2",
+    "url": "/checkers-react/static/js/main.51b9cad4.chunk.js"
+  },
+  {
+    "revision": "fa1abbc2dbbf725216f2",
+    "url": "/checkers-react/static/js/runtime-main.8d7c0e39.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/checkers-react/static/media/logo.25bf045c.svg"
+  }
+]);
